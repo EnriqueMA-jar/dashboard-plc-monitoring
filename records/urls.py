138ec28a5f583f8af3f80739106c06b66255de56
Enrique Saraiba <LIS21110783@irapuato.tecnm.mx>
2025-06-26 08:45:28 -0600

    New templates

--- records/urls.py
+++ records/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.records_view, name='records'),
-    path('records', views.records_view, name='records'),
+    path('records/', views.records_view, name='records'),
 ]
 
--- records/views.py
+++ records/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def records_view(request):
-    return render(request, 'records/datos.html')
+    return render(request, 'records/records.html')
