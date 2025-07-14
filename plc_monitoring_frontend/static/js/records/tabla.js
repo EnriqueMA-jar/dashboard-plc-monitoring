f680b120db2e68d3fe0dcfcb8f9b64324e9f230d
Enrique Saraiba <LIS21110783@irapuato.tecnm.mx>
2025-07-14 12:58:45 -0600

    records is barely fixed

--- plc_monitoring_frontend/static/js/records/tabla.js
+++ plc_monitoring_frontend/static/js/records/tabla.js
@@ -1,11 +1,6 @@
 let dataTable;
-let dataTableIsInitialized = false;
 
-const initiDataTable = async () => {
-  if (dataTableIsInitialized) {
-    dataTable.destroy();
-  }
-
+const initDataTable = async () => {
   await listCharges();
 
   dataTable = $("#datatable_charges").DataTable({
@@ -15,37 +10,38 @@ const initiDataTable = async () => {
       url: "//cdn.datatables.net/plug-ins/1.13.4/i18n/es-ES.json"
     }
   });
-
-  dataTableIsInitialized = true;
 };
-const listCharges=async()=>{
-    try{
-        const response = await fetch("http://127.0.0.1:8001/api/Charges/list/", { credentials: 'include' });
-        const charges = await response.json();
-        
-        let content = ``;
-        charges.forEach((charges, index)=> {
-            content +=`
-            <tr>
-                <td>${index + 1}</td>
-                <td>${charges.charge_date}</td>
-                <td>${charges.charge_time_start}</td>
-                <td>${charges.charge_time_finish}</td>
-                <td>${charges.charge_weight_start}</td>
-                <td>${charges.charge_weight_finish}</td>
-                <td>${charges.charge_humidity_start}</td>
-                <td>${charges.charge_humidity_finish}</td>
-                <td>${charges.charge_temperature_start}</td>
-                <td>${charges.charge_temperature_finish}</td>
-            </tr>
-            `;
-        });
-        tableBody_charges.innerHTML = content;
-    }catch(error){
-        alert("Error al cargar los datos: " + error);
-    }
-}
 
-window.addEventListener("load", async () => {
-    await initiDataTable();
+const listCharges = async () => {
+  try {
+    const response = await fetch("http://127.0.0.1:8001/api/Charges/list/", {
+      credentials: 'include'
+    });
+    const charges = await response.json();
+
+    let content = ``;
+    charges.forEach((charges, index) => {
+      content += `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${charges.charge_date}</td>
+            <td>${charges.charge_time_start}</td>
+            <td>${charges.charge_time_finish}</td>
+            <td>${charges.charge_weight_start}</td>
+            <td>${charges.charge_weight_finish}</td>
+            <td>${charges.charge_humidity_start}</td>
+            <td>${charges.charge_humidity_finish}</td>
+            <td>${charges.charge_temperature_start}</td>
+            <td>${charges.charge_temperature_finish}</td>
+        </tr>
+      `;
+    });
+    tableBody_charges.innerHTML = content;
+  } catch (error) {
+    alert("Error al cargar los datos: " + error);
+  }
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+  await initDataTable();
 });
